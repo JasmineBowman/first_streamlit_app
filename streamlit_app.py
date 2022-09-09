af67e671aae0b7107dbdd3891253ab51b8b3c3c0
JB <34419452+JasmineBowman@users.noreply.github.com>
2022-09-09 16:08:28 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,13 +12,10 @@ streamlit.text('🐔 Hard-Boiled Free Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect ("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
 
 # REPEATBLE CODE BLOC (FUNCTION) 
@@ -39,8 +36,6 @@ try:
          
 except URLError as e:
   streamlit.error()
-
-#my_data_rows = my_cur.fetchone()
 
 streamlit.header("The Fruit Load List contains:")
 #Snowflake Related Functions
